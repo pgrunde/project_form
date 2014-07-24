bed29b6fdb2b8698358793446e2c6b534a97ecbe
Peter Grunde <peter.a.grunde@gmail.com>
2014-07-24 16:07:07 -0600

    fixed paragraph margin issue in #stage

--- public/form_script.js
+++ public/form_script.js
@@ -1,35 +1,35 @@
 $(document).ready(function() {
 
   $('#contact_info_selector').click(function() {
-    $('#stage').empty().append("<p>Contact Info</p>");
+    $('#stage').empty().append("<span>Contact Info</span>");
   });
 
   $('#text_selector').click(function() {
-    $('#stage').empty().append("<p>Text</p>");
+    $('#stage').empty().append("<span>Text</span>");
   });
 
   $('#textarea_selector').click(function() {
-    $('#stage').empty().append("<p>Text Area</p>");
+    $('#stage').empty().append("<span>Text Area</span>");
   });
 
   $('#checkbox_selector').click(function() {
-    $('#stage').empty().append("<p>Checkbox</p>");
+    $('#stage').empty().append("<span>Checkbox</span>");
   });
 
   $('#radio_selector').click(function() {
-    $('#stage').empty().append("<p>Radio</p>");
+    $('#stage').empty().append("<span>Radio</span>");
   });
 
   $('#date_selector').click(function() {
-    $('#stage').empty().append("<p>Date</p>");
+    $('#stage').empty().append("<span>Date</span>");
   });
 
   $('#dropdown_selector').click(function() {
-    $('#stage').empty().append("<p>Dropdown</p>");
+    $('#stage').empty().append("<span>Dropdown</span>");
   });
 
   $('#file_selector').click(function() {
-    $('#stage').empty().append("<p>Add File</p>");
+    $('#stage').empty().append("<span>Add File</span>");
   });
 
 });
