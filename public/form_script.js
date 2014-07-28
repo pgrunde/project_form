fd342463fed84a4d38afc47439f2fc0fd45de82c
Peter Grunde <peter.a.grunde@gmail.com>
2014-07-27 22:10:37 -0600

    partially done form input jQuery z-index control

--- public/form_script.js
+++ public/form_script.js
@@ -1,35 +1,83 @@
 $(document).ready(function() {
 
   $('#contact_info_selector').click(function() {
-    $('#stage').empty().append("<span>Contact Info</span>");
+    $('#stage_neutral').css("z-index","1");
   });
 
   $('#text_selector').click(function() {
-    $('#stage').empty().append("<span>Text</span>");
+    $('#staged_text').css("z-index","1");
+    $('#staged_text_area').css("z-index","-1");
+    $('#staged_checkbox').css("z-index","-1");
+    $('#staged_radio').css("z-index","-1");
+    $('#staged_date').css("z-index","-1");
+    $('#staged_dropdown').css("z-index","-1");
+    $('#stage_neutral').css("z-index","-1");
+
+    $('#final_form').empty().append("text")
   });
 
   $('#textarea_selector').click(function() {
-    $('#stage').empty().append("<span>Text Area</span>");
+    $('#staged_text_area').css("z-index","1");
+    $('#staged_text').css("z-index","-1");
+    $('#staged_checkbox').css("z-index","-1");
+    $('#staged_radio').css("z-index","-1");
+    $('#staged_date').css("z-index","-1");
+    $('#staged_dropdown').css("z-index","-1");
+    $('#staged_neutral').css("z-index","-1");
+
+    $('#final_form').empty().append("textarea")
   });
 
   $('#checkbox_selector').click(function() {
-    $('#stage').empty().append("<span>Checkbox</span>");
+    $('#staged_checkbox').css("z-index","1");
+    $('#staged_text').css("z-index","-1");
+    $('#staged_text_area').css("z-index","-1");
+    $('#staged_radio').css("z-index","-1");
+    $('#staged_date').css("z-index","-1");
+    $('#staged_dropdown').css("z-index","-1");
+    $('#staged_neutral').css("z-index","-1");
+
+    $('#final_form').empty().append("checkbox")
   });
 
   $('#radio_selector').click(function() {
-    $('#stage').empty().append("<span>Radio</span>");
+    $('#staged_radio').css("z-index","1");
+    $('#staged_text').css("z-index","-1");
+    $('#staged_text_area').css("z-index","-1");
+    $('#staged_checkbox').css("z-index","-1");
+    $('#staged_date').css("z-index","-1");
+    $('#staged_dropdown').css("z-index","-1");
+    $('#staged_neutral').css("z-index","-1");
+
+    $('#final_form').empty().append("radio")
   });
 
   $('#date_selector').click(function() {
-    $('#stage').empty().append("<span>Date</span>");
+    $('#staged_date').css("z-index","1");
+    $('#staged_text').css("z-index","-1");
+    $('#staged_text_area').css("z-index","-1");
+    $('#staged_checkbox').css("z-index","-1");
+    $('#staged_radio').css("z-index","-1");
+    $('#staged_dropdown').css("z-index","-1");
+    $('#staged_neutral').css("z-index","-1");
+
+    $('#final_form').empty().append("date")
   });
 
   $('#dropdown_selector').click(function() {
-    $('#stage').empty().append("<span>Dropdown</span>");
-  });
+    $('#staged_dropdown').css("z-index","1");
+    $('#staged_text').css("z-index","-1");
+    $('#staged_text_area').css("z-index","-1");
+    $('#staged_checkbox').css("z-index","-1");
+    $('#staged_radio').css("z-index","-1");
+    $('#staged_date').css("z-index","-1");
+    $('#staged_neutral').css("z-index","-1");
 
-  $('#file_selector').click(function() {
-    $('#stage').empty().append("<span>Add File</span>");
+    $('#final_form').empty().append("dropdown")
   });
+//
+//  $('#file_selector').click(function() {
+//    $('#stage').empty().append("<span>Add File</span>");
+//  });
 
 });
